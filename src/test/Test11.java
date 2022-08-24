5b6d769e3d87ac73d93d284ae8f4bbbf91e0ca56
ramch <ramch@ramlaxman>
2022-08-24 10:49:02 +0530

    "First Commit"

---
+++ src/test/Test11.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Test11 {
+
+	public static void main(String[] args) {
+
+
+		String a=null;
+		
+		System.out.println(a);
+		/*try
+		{
+			System.out.println(a.length());
+		}*/
+
+	}
+
+}
